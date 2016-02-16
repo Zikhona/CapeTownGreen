c616c78a40ae77fb84ff4bdcba07044c57fd70b4
Zikhona <zikhonalaurettapoto35@gmail.com>
2016-02-16 15:59:48 +0200

    First Function

--- app.js
+++ app.js
@@ -1,6 +1,33 @@
+ // example* button = document.getElementById(buttonId);
 
-var body = document.querySelector("body");
+ // if (className === "failing") {
+ // 	myButton.innerHTML = "Make passing";
+ // }
+ // else {
+ // 	myButton.innerHTML = "Make failing";
+ // }
+ //    };
+var keyCodeName = function(keyCode){
+if(keyCode=== 38){
+		return "up";
+	}
+if(keyCode=== 39){
+		return "right";
+	}
+if(keyCode===40){
+		return "down";
+	}
+if(keyCode=== 37){
+		return "left";
+	}
+else {
+		return "";
+	}
+};
+
+ var body = document.querySelector("body");
 
 body.onkeydown = function(e){
     //e.keyCode - will capture the key codes
+
 };
