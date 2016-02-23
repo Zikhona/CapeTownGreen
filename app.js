58d1155a1be61caa87f3eda966630f59fd395e91
Zikhona <zikhonalaurettapoto35@gmail.com>
2016-02-23 15:15:10 +0200

    moving

--- app.js
+++ app.js
@@ -1,81 +1,64 @@
 var body = document.querySelector("body");
 
-// var taxiLocationCounter = 1;
+	var taxiLocationCounter = 1;
 
+  body.onkeydown = function(e){
 
-// body.onkeydown = function(e){
-	
-// 	// e.keyCode - will capture the key codes
-// 	// displayMessage(e.keyCode);
-
-// 	  var count = keyCodeName(e.keyCode);
-// 	   // displayMessage(right);
-
-// 	   var myClass = createLocationClass(taxiLocationCounter);
-// 	   displayMessage(myClass);
-// };
-
-// if (count === "right") {
-// 	taxiLocationCounter++;
-// };
-// else {
-// 	taxiLocationCounter--;
-// };
-	
-var keyCodeName = function (keyCode){
-if(keyCode=== 38){
+ 	  var right = keyCodeName(e.keyCode);
+ 	   		var moveForward = function(){
+ 	   			var createLocationClass(taxiLocationCounter);
+ 	   			var currrentLocation;
+ 	   			var newLocation;
+ 	   		};
+ displayMessage(number);
+};
+var keyCodeName = function (keyCode) {
+	if(keyCode === 38) {
 		return "up";
 	}
-if(keyCode=== 39){
+	if(keyCode === 39) {
 		return "right";
 	}
-if(keyCode===40){
+	if(keyCode === 40) {
 		return "down";
 	}
-if(keyCode=== 37){
+	if(keyCode === 37) {
 		return "left";
 	}
-else {
+	else {
 		return "";
 	}
 };
 
-
-var createLocationClass = function(number) {
+var createLocationClass = function (number) {
 	if(number === 1) {
-	return "slot-one-of-nine";
+		return "slot-one-of-nine";
 	}
-
-	else if(number === 2) {
-	return "slot-two-of-nine";
-	}	
-
-	else if(number === 3) {
-	return "slot-three-of-nine";
-	}	
-
-	else if(number === 4) {
-	return "slot-four-of-nine";
-	}	
-
-	else if(number === 5){
-	return "slot-five-of-nine";
-	}	
-
-	else if(number === 6){
-	return "slot-two-of-nine";
-	}	
-
-	else if(number === 7){
-	return "slot-two-of-nine";
+	if(number === 2) {
+		return "slot-two-of-nine";
 	}
-
-	else if(number === 8){
-	return "slot-eight-of-nine";
-	}	
-
-	else if(number === 9){
-	return "slot-nine-of-nine";
-	}		
-
+	if(number === 3) {
+		return "slot-three-of-nine";
+	}
+	if(number === 4) {
+		return "slot-four-of-nine";
+	}
+	if(number === 5) {
+		return "slot-five-of-nine";
+	}
+	if(number === 6) {
+		return "slot-six-of-nine";
+	}
+	if(number === 7) {
+		return "slot-seven-of-nine";
+	}
+	if(number === 8) {
+		return "slot-eight-of-nine";
+	}
+	if(number === 9) {
+		return "slot-nine-of-nine";
+	}
+	else {
+		return "";
+	}
 };
